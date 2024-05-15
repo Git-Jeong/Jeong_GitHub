915abfe5515dcb5ae95aa41cb249493271995275
JeongJin <jeongjin9427@gmail.com>
2024-05-15 20:35:06 +0900

    react

---
+++ MERN_Study/react 스폰/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import chatGPT_gui from './chatGPT_gui';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <chatGPT_gui />
+      </header>
+    </div>
+  );
+}
+
+export default App;
